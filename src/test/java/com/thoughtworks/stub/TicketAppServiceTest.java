5c88c9db345a07f52de3779e0c61721bdca233c6
jason <lin.li@thoughtworks.com>
2019-04-12 22:45:33 +0800

    stub

--- src/test/java/com/thoughtworks/stub/TicketAppServiceTest.java
+++ src/test/java/com/thoughtworks/stub/TicketAppServiceTest.java
@@ -1,13 +1,38 @@
-//package com.thoughtworks.stub;
-//
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
-//@SpringBootApplication
-//@AutoConfigureWireMock
-//public  class TicketAppServiceTest {
-//    public static void main(String[] args) {
-//        SpringApplication.run(TicketAppServiceTest.class, args);
-//    }
-//}
+package com.thoughtworks.stub;
+
+        import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureStubRunner(ids = {"com.thoughtworks:parkinglot:0.0.1-SNAPSHOT:stubs:8081"}, //引入jar 包 parkinglot-0.0.1-SNAPSHOT-stubs.jar
+        stubsMode = StubRunnerProperties.StubsMode.LOCAL)
+//一定要将stub的jar包安装到本地，或者使用远程
+public class TicketAppServiceTest {
+
+    @Autowired
+    MockMvc mockMvc;
+    @Test
+    public void test() throws Exception {
+       mockMvc.perform(MockMvcRequestBuilders.post("/tickets")
+                .contentType(MediaType.APPLICATION_JSON_UTF8).content("{\"car\":\"my car\",\"spotId\":1,\"lotId\":2}")
+        ).andExpect(status().is(201));
+
+
+
+    }
+
+}
