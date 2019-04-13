cc55cb079cd232c1a84a0d2e6c7f95ec30431960
Ruijie Huang <ruijieh20@gmail.com>
2019-04-13 13:54:30 +0800

    use config to get ticket-service

--- src/main/java/com/thoughtworks/parkinglot/infrastructure/web/TicketService.java
+++ src/main/java/com/thoughtworks/parkinglot/infrastructure/web/TicketService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "ticket-service", url = "localhost:8080")
+@FeignClient(name = "ticket-service", url = "${ticket-service.url}")
 @Component
 public interface TicketService {
 
